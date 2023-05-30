db0457b34c81b143ef76602ff832748fbe92c318
Sanam_App <ugyendechen00@gmail.com>
2023-05-30 10:08:28 +0000

    db change

--- myapp/datastore/postgres/db.go
+++ myapp/datastore/postgres/db.go
@@ -11,30 +11,31 @@ import (
 //db details
 
 const (
-	postgres_host  = "db"
-	postgres_port  = 5432
-	postgres_user  = "postgres"
-	postgres_password  = "postgres"
-	postgres_dbname   = "my_db"
+	postgres_host     = "dpg-chqrtc7dvk4goeu8of9g-a.singapore-postgres.render.com"
+	postgres_port     = 5432
+	postgres_user     = "postgres_admin"
+	postgres_password = "XkTGi9dbW2ro5Tbi6BVIH3zysU1ooCqs"
+	postgres_dbname   = "my_db_rzx2"
 )
 
 //create pointer variable Db which points to sql driver
 
 var Db *sql.DB
+
 // init() is always called before main()
 
 func init() {
 	//creating the connection string
-	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",postgres_host,postgres_port,postgres_user,postgres_password,postgres_dbname)
+	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", postgres_host, postgres_port, postgres_user, postgres_password, postgres_dbname)
 
 	var err error
 	//open connection to database
 
-	Db, err = sql.Open("postgres",db_info)
+	Db, err = sql.Open("postgres", db_info)
 
 	if err != nil {
 		panic(err)
-	}else {
+	} else {
 		log.Println("Database successfully configured")
 	}
 }
